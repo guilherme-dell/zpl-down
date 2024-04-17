b9607339d8812f6d8f72985d618522a1298fa740
guilherme-dell <guithronestudo@hotmail.com>
2024-04-17 03:22:28 -0300

    zpl-down 0.2

--- zpl/barcode.go
+++ zpl/barcode.go
@@ -55,14 +55,3 @@ func (b Barcode) Download() error {
 
 	return nil
 }
-
-func createDir() {
-	dir := "../codigos_gerados"
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err := os.MkdirAll(dir, 0755)
-		if err != nil {
-			fmt.Println("Erro ao criar diretorio.")
-			panic(err)
-		}
-	}
-}
